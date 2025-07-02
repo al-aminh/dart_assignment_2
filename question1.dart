71f3074c3e7a97cc9261306faa7286744f15bebf
al-aminh <alaminhossain123245@gmail.com>
2025-07-02 22:53:59 +0600

    question 1

---
+++ question1.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+void main(){
+  int? number = int.parse(stdin.readLineSync()!);
+  if(number % 2 == 0){
+    print("$number is Even");
+  } else {
+    print("$number is Odd");
+  }
+}
